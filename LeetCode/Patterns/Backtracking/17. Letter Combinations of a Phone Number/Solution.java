022e6ef63e585809179ec9e3bbfb2605b707a185
PrasannaVenkatesanE <prasanna.108411913@gmail.com>
2026-09-05 01:00:04 +0530

    Add Medium Java Solution for 17. Letter Combinations of a Phone Number from LeetCode

--- LeetCode/Patterns/Backtracking/17. Letter Combinations of a Phone Number/Solution.java
+++ LeetCode/Patterns/Backtracking/17. Letter Combinations of a Phone Number/Solution.java
@@ -1,41 +1,26 @@
-import java.util.*;
-
 class Solution {
     public List<String> letterCombinations(String digits) {
-        List<String> res = new ArrayList<>();
-        
-        // Edge case: if input is empty, return empty list immediately
-        if (digits == null || digits.length() == 0) {
-            return res;
-        }
-
-        // Map setup
+        ArrayList<String> res = new ArrayList<>();
         String[][] arr = {{"2","abc"},{"3","def"},{"4","ghi"},{"5","jkl"},{"6","mno"},{"7","pqrs"},{"8","tuv"},{"9","wxyz"}};
-        Map<Character, String> map = new HashMap<>(); // Changed key to Character for easier lookups
-        for (int i = 0; i < arr.length; i++) {
-            map.put(arr[i][0].charAt(0), arr[i][1]);
+        Map<String,String> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i][0],arr[i][1]);
         }
+        
+        if (digits.length() != 0){
+            backTrack(0,"",digits, res,map);
 
-        // Start backtracking
-        backTrack(0, "", digits, res, map);
+        }
         return res;
-    }    
-
-    // The backtracking method must be declared outside, at the class level
-    private void backTrack(int i, String curStr, String digits, List<String> res, Map<Character, String> map) {
-        // Base case: if current string length matches digits length, we found a combination
-        if (curStr.length() == digits.length()) {
-            res.add(curStr);
-            return;
-        }
-        
-        // Get the letters corresponding to the current digit
-        char currentDigit = digits.charAt(i);
-        String letters = map.get(currentDigit);
-        
-        // Loop through the letters and recurse
-        for (char c : letters.toCharArray()) {
-            backTrack(i + 1, curStr + c, digits, res, map);
-        }
     }
+    static void backTrack(int i,String curStr,String digits, List<String> res,Map<String,String> map){
+            if(curStr.length() == digits.length()){
+                res.add(curStr);
+                return;
+            }
+            String value = String.valueOf(digits.charAt(i));
+            for(char c : map.get(value).toCharArray()){
+                backTrack(i+1, curStr+c, digits , res,map);
+            }
+        }
 }
