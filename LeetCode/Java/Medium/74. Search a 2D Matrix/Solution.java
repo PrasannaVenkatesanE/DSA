dd4fcc6d217840dda8cab6bd8e97d70de26c3ab0
PrasannaVenkatesanE <prasanna.108411913@gmail.com>
2026-09-10 09:39:13 +0530

    Add Medium Java Solution for 74. Search a 2D Matrix from LeetCode

---
+++ LeetCode/Java/Medium/74. Search a 2D Matrix/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for(int i=0;i<matrix.length;i++){
+            int low = 0;
+            int high = matrix[i].length - 1;
+
+            while(low <= high){
+                int middle = (low + high) / 2;
+                if(matrix[i][middle] == target){
+                    return true;
+                }
+                if(target > matrix[i][middle]){
+                    low = middle+1;
+                }
+                else{
+                    high = middle-1;
+                }
+            }
+        }
+        return false;
+    }
+    
+}
