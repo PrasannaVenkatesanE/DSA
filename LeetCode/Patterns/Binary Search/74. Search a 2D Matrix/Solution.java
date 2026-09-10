703430112cabed29d78d51f71334d0df63ed6eb9
PrasannaVenkatesanE <prasanna.108411913@gmail.com>
2026-09-10 09:56:06 +0530

    Add Medium Java Solution for 74. Search a 2D Matrix from LeetCode

--- LeetCode/Patterns/Binary Search/74. Search a 2D Matrix/Solution.java
+++ LeetCode/Patterns/Binary Search/74. Search a 2D Matrix/Solution.java
@@ -1,25 +1,40 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int i=0;i<matrix.length;i++){
-            int low = 0;
-            int high = matrix[i].length - 1;
-            if(target < matrix[i][low] && target > matrix[i][high]){
-                continue;
+        
+        int l = 0;
+        int h = matrix.length;
+        int targetRow = 0;
+
+        while(l<=h){
+            int mid = (l + h) / 2;
+            if(target >= matrix[mid][0] && target <= matrix[mid][matrix[mid].length-1]){
+                targetRow = mid;
+                break;
             }
-            while(low <= high){
-                int middle = (low + high) / 2;
-                if(matrix[i][middle] == target){
-                    return true;
-                }
-                if(target > matrix[i][middle]){
-                    low = middle+1;
-                }
-                else{
-                    high = middle-1;
-                }
+            if(target > matrix[mid][matrix[mid].length-1]){
+                l = mid + 1;
+            }
+            else{
+                h = mid - 1;
             }
         }
+        int low = 0;
+        int high = matrix[targetRow].length - 1;
+        while(low <= high){
+            int middle = (low + high) / 2;
+            if(matrix[targetRow][middle] == target){
+                return true;
+            }
+            if(target > matrix[targetRow][middle]){
+                low = middle+1;
+            }
+            else{
+                high = middle-1;
+            }
+        }
+        
         return false;
     }
+    
     
 }
