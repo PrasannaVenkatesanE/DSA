17f91949648fa047d46d93f0bd862e446cc75d88
PrasannaVenkatesanE <prasanna.108411913@gmail.com>
2026-09-10 11:13:33 +0530

    Add Medium Java Solution for 240. Search a 2D Matrix II from LeetCode

--- LeetCode/Java/Medium/240. Search a 2D Matrix II/README.md
+++ LeetCode/Java/Medium/240. Search a 2D Matrix II/README.md
@@ -8,8 +8,8 @@
 Array, Binary Search, Divide and Conquer, Matrix
 
 ### 🚀 Performance
-- **Runtime:** 4 ms
-- **Memory:** 48.2 MB
+- **Runtime:** 3 ms
+- **Memory:** 48.3 MB
 
 ---
 
--- LeetCode/Patterns/Binary Search/240. Search a 2D Matrix II/Solution.java
+++ LeetCode/Patterns/Binary Search/240. Search a 2D Matrix II/Solution.java
@@ -1,21 +1,17 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        
-        for(int i=0;i<matrix.length;i++){
-            int left = 0;
-            int right = matrix[i].length - 1;
+        int row = 0;
+        int col = matrix[0].length - 1;
 
-            while(left <= right){
-                int mid = (left + right) / 2;
-                if(matrix[i][mid] == target){
-                    return true;
-                }
-                else if(matrix[i][mid] < target){
-                    left = mid + 1;
-                }
-                else{
-                    right = mid - 1;
-                }
+        while(row < matrix.length && col >=0){
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] > target){
+                col--;
+            }
+            else{
+                row++;
             }
         }
         return false;
