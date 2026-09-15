d78ef332605aaea86d5dfa28c6c781baae73bcc2
PrasannaVenkatesanE <prasanna.108411913@gmail.com>
2026-09-15 17:25:15 +0530

    Add Hard Java Solution for 2472. Maximum Number of Non-overlapping Palindrome Substrings from LeetCode

---
+++ LeetCode/Patterns/Dynamic Programming/2472. Maximum Number of Non-overlapping Palindrome Substrings/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxPalindromes(String s, int k) {
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+            for(int j=i;j<s.length()+1;j++){
+                String str = s.substring(i,j);
+                if(str.length()>=k){
+                    System.out.println(str);
+                    if(isPalindrome(str)){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+    boolean isPalindrome(String str){
+        int left = 0;
+        int right = str.length()-1;
+
+        while(left< right){
+            if(str.charAt(left) != str.charAt(right)){
+                return false;
+            }
+            left++;right--;
+        }
+        return true;
+    }
+}
