d7a511bca0a3750f97e99ca6844afd59836498d8
PrasannaVenkatesanE <prasanna.108411913@gmail.com>
2026-09-16 13:54:21 +0530

    Add Easy Java Solution for 342. Power of Four from LeetCode

--- LeetCode/Java/Easy/342. Power of Four/Solution.java
+++ LeetCode/Java/Easy/342. Power of Four/Solution.java
@@ -1,9 +1,12 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
+        
         if(n == 1){
             return true;
         }
-
+        if(n < 0){
+            n = n * -1;
+        }
         while(n > 1){
             if(n%4 != 0){
                 return false;
